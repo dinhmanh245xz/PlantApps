a48a62317050c32d95eb9893f00fb3f9cd13ae47
dinhmanh245xz <61041412+dinhmanh245xz@users.noreply.github.com>
2022-04-28 00:32:22 +0700

    update passing data

--- App.js
+++ App.js
@@ -4,21 +4,18 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import HomeScreen from './src/screen/HomeScreen';
-import Screen1 from './src/screen/Screen1';
-import Screen2 from './src/screen/Screen2';
-import Login from './src/screen/Login';
 
+import Login from './src/Screen/Login';
+import profile from './src/Screen/profile';
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen name="HomeScreen" component={HomeScreen} /> */}
+       
         <Stack.Screen name='Login' component={Login}/>
-        <Stack.Screen name="Screen1" component={Screen1} />
-        <Stack.Screen name="Screen2" component={Screen2} />
+        <Stack.Screen name="profile" component={profile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,11 +1,37 @@
-import { View, Text,TextInput,StyleSheet ,Dimensions} from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { View, Text,TextInput,StyleSheet ,Dimensions,TouchableOpacity} from 'react-native'
 
-export default function Login() {
+
+export default function Login({navigation}) {
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  const Submit=() => {
+    navigation.navigate('profile', {
+      userName : name,
+      userEmail :email,
+      userPassword : password,
+    });
+  };
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-     <Text>Email</Text>
-     <TextInput style={style.input} placeholder='Email' />
+      <View>
+      <Text style={{textAlign:'center',alignItems:'center'}}>Login</Text>
+     <TextInput style={style.input} placeholder='user' placeholderTextColor={'#ccc'}
+      value={name} onChangeText ={(text) => setName(text)} />
+    
+     <TextInput style={style.input} placeholder='Emal' placeholderTextColor={'#ccc'}
+     value={email} onChangeText={(text) => setEmail(text)} />
+     
+     <TextInput style={style.input} placeholder='Password' placeholderTextColor={'#ccc'}
+     value={password} onChangeText={(text) => setPassword(text)} />
+
+     <TouchableOpacity style={{justifyContent:'center',alignItems:'center',backgroundColor:'lightblue'}}
+     onPress={() => Submit()}>
+       <Text>Submit</Text>
+     </TouchableOpacity>
+      </View>
     </View>
   )
 };
@@ -14,5 +40,9 @@ const {width} = Dimensions.get('screen')
 const style = StyleSheet.create ({
     input : {
         width :width-20,
+        borderWidth:1,
+        marginTop:10,
+        padding:10,
+        borderRadius:20,
     },
 })
